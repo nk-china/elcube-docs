f4d05a395f2f3307bf79bf59eb58de1f2f46bd09
bean <mailxbs@126.com>
2021-12-02 16:28:15 +0800

    Easis Docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,15 +2,26 @@ module.exports = {
     port: 8200,
     dest: './dist',
     theme: '@vuepress/vue',
+    head: [
+        ['link', { rel: 'icon', href: `/easis.logo.min.png` }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ],
     locales: {
         '/': {
             lang: 'zh-CN',
-            title: '企业级快速开发模型',
+            title: 'EAsis 企业级快速开发模型',
             description: '企业级快速开发模型'
         },
         '/en/': {
             lang: 'en-US',
-            title: '企业级快速开发模型',
+            title: 'EAsis 企业级快速开发模型',
             description: '企业级快速开发模型'
         }
     },
